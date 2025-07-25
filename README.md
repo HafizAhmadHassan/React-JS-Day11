ee3da6f2729c8b57ad7d18a7080e088978bde332
developKgn <develop@kgn.it>
2025-07-25 12:35:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
-# Lec 20
+# Lec 21
 
-- Input filed
-- Define state
-- input field in state
-- Display Value
-- Button Click Interaction
+- What is controlled component
+- identify controlled component
+- Error if we dont use controlled value property
+- Make form get input fileds values
+- Interview Question
+
+- A controlled component is form whose input field values are controlled by React's State
+
+- Here how it works:
+    - Strore input field value in State
+    - Use Change handler with input field
+    - value attribute attached with State
+
+Benefits
+    - Single Source of Truth
+    - Validation and Manipulation before Submit
+    - Dynamic Updates Values
+
+Arrow function vs Normal
+- this is Lexical it will look for global
+for example
+let object{
+    x:2
+    function normal(){
+        return this.x
+    }
+    function arrow()=>{
+        return this.x # this does not work
+    }
+
+}
+
+-  normal can consume arguemnts
+- return in implicit if it is single line in arrow function
+
+- Function redefined constantly if it arrow function wheen Component rerenders  See in code
+incerementCount Function in that case we should use normal function
+- console .log is added check in console
+
+Explore the best : https://www.youtube.com/watch?v=kmYxpgHbkoY
--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,47 @@ import PropsCondition from './PropsCondition';
 
 
 function App() {
-  const [val,setVal]=useState("")
+  const [val,setVal]=useState("");
+  
+  const [Name,setName]=useState("");
+
+  const [Password,setPassword]=useState("")
+  const [Email,setEmail]=useState("")
+
   const [count, setCount] = useState(0);
+  
+  
+  const [incount, setinCount] = useState(0);
+  const incerementCount=()=>{
+      console.log(incount)
+      alert(incount)
+      setinCount(incount+1)
+  }
+
+  const [incount2, setinCount2] = useState(0);
+  function incerementCount2(){
+      console.log(incount2)
+      alert(incount2)
+      setinCount2(incount2+1)
+  }
+
+  const myFunctionToExploreArrow=()=>{
+    console.log("Button Clicked")
+    alert("Button Clicked")
+
+  }
+
+  const myFunctionToExploreArrowArguemtns=(count)=>{
+    console.log(count,"Button Clicked")
+
+  }
+  const [myState,setmyState]= useState(false)
+  const myFunctionToExploreArrowArguemtnsWithState=(count)=>{
+    console.log(count,"Button Clicked")
+    alert("Button Clicked")
+    setmyState(!myState)
+
+  }
   /* alert(sum()) */
 
 /* This is code modification/added for Lec 14-15 */
@@ -25,6 +64,8 @@ function App() {
   
   const [Display, setDisplay] = useState(true);
   
+  
+
   const handleFruit=()=>{
     setFruit("Banana")
   }
@@ -152,6 +193,64 @@ if display variable exist print heading else null */}
       <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder='Enter username' />
       <h1>{val}</h1>
       <button onClick={()=>setVal("")}>Clear</button>
+
+       {/* This is code modification for Lec 20*/}
+       
+       {/* This is code modification for Lec 21*/}
+
+       <h1> Controller Component</h1>
+       <form action="" method="get">
+        <input type="text" value={Name} onChange={(event)=>setName(event.target.value)} placeholder='Enter Name'/>
+        <br /> <br />
+        <input type="text" value={Password} onChange={(event)=>setPassword(event.target.value)} placeholder='Enter password'/>
+       
+        <br /> <br />
+        <input type="text" value={Email} onChange={(event)=>setEmail(event.target.value)} placeholder='Enter email'/>
+        <br /> <br />
+        <button>Submit</button>  
+        <button onClick={()=>{setEmail("");setPassword("");setEmail("")}}>Clear</button>
+        <h3>{Name}</h3>
+        <h3>{Password}</h3>
+        <h3>{Email}</h3>     
+       </form>
+
+      {/* The difference Between Arrow Function and Normal*/}
+        <h1>The difference Between Arrow Function and Normal</h1>
+        <h2> Arrow Function Constantly redecreate when render  count : {incount}</h2>
+        <button onClick={incerementCount}> Increment Count</button>
+        <h2> Normal Function make sure it does not recreated constantly count : {incount2}</h2>
+        <button onClick={incerementCount2}> Increment Count</button>
+      
+      <div>
+      {/* This will work but when to pass arguemnt is does not work*/}
+
+        <button onClick={myFunctionToExploreArrow}>Click</button>
+      </div>
+
+
+      <div>
+
+      {/* button will not work :<button onClick={myFunctionToExploreArrowArguements(20)}></button>*/}
+      {/* Even if we pass arguments */}
+
+        
+      </div>
+
+      <div>
+
+      {/* button will not work :<button onClick={myFunctionToExploreArrowArguementsWithState(20)}></button>*/}
+      {/* Even if we add states it will terminate infinitely execute because it rerenders infinitely */}
+
+        
+      </div>
+
+      <div>
+      {/* To make it Work */}
+
+      <button onClick={()=>myFunctionToExploreArrowArguemtnsWithState(20)}>Click Work with() </button>
+
+        
+      </div>
     </>
   );
 }
